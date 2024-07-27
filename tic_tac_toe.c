f721169fe6bb28a9e7e683475790bbb394710abb
Maximilian Stockmann <maximilian.stockmann@posteo.de>
2024-07-27 18:24:13 +0100

    add functions for board management, introduce segfault

--- tic_tac_toe.c
+++ tic_tac_toe.c
@@ -1,9 +1,61 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
+#include <ncurses.h>
 
+#define X_MARK 88
+#define O_MARK 79
+
+typedef struct {
+  int mark; /* can be x or o */
+  int cursor; /* can be 1 or 0 */
+} Cell;
+
+typedef struct {
+  Cell cells[2][2];
+} Board;
+
+void render_board();
+void init_board();
+
+// have a nine by nine array
+// render the array upon each input as some series of symbols
+// symbols to signify marked squares, background fill to signify cursor position
+// upon using one of the arrow keys change cursor location in array
 int main(int argc, char *argv[])
 {
-  printf("%s\n", "Hello world!");
+  printf("%c", X_MARK);
+  int c; /* the character read*/
+  Board board;
+  Board* board_ptr = &board;
+
+  init_board(board_ptr);
+
+  while ((c = getchar()) != EOF)
+  {
+    render_board(board_ptr);
+  }
+
   return EXIT_SUCCESS;
+}
+
+void render_board(Board *board) {
+    for (int row = 0; row <= 2; row++)
+    {
+      for (int col = 0; col <= 2; col++)
+      {
+        putchar(board->cells[row][col].mark);
+      }
+      putchar(10); // LF
+      clear();
+    }
+    refresh();
+}
+
+void init_board(Board *board) {
+  for (int row = 0; row <= 2; row++) {
+    for (int col = 0; col <= 2; col++) {
+      board->cells[row][col].mark = X_MARK;
+    }
+  }
 }
