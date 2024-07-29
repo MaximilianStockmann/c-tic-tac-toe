cb7a81a366be4a0839a8b15160ad780dd94f3a95
Maximilian Stockmann <maximilian.stockmann@posteo.de>
2024-07-29 19:10:38 +0100

    refactor unnessecary struct Screen_Position for the program to use ncurses utilities instead

--- tic_tac_toe.c
+++ tic_tac_toe.c
@@ -15,21 +15,18 @@ typedef struct {
   Cell cells[3][3];
 } Board;
 
-typedef struct {
-  int x;
-  int y;
-} Screen_Position;
-
-void render_board();
+void render_board(Board *board);
 void init_board();
 void update_cell(Board *board, int row, int col, char symbol);
 void init_ncurses();
-void move_cursor(Screen_Position *scrpos, int x, int y);
-void move_cursor_up(Screen_Position *scrpos);
-void move_cursor_down(Screen_Position *scrpos);
-void move_cursor_left(Screen_Position *scrpos);
-void move_cursor_right(Screen_Position *scrpos);
-void handle_input(int ch, Screen_Position *scrpos, Board *board_ptr);
+void move_cursor(int y, int x);
+void move_cursor_up();
+void move_cursor_down();
+void move_cursor_left();
+void move_cursor_right();
+void handle_input(int ch, Board *board);
+void print_banner();
+void game_loop(Board *board);
 
 // have a nine by nine array
 // render the array upon each input as some series of symbols
@@ -39,16 +36,26 @@ int main(int argc, char *argv[])
 {
   init_ncurses();
   int c; /* the character read*/
-  Board *board_ptr = malloc(sizeof *board_ptr);
-  Screen_Position *scrpos = malloc(sizeof *scrpos);
+  Board *board= malloc(sizeof *board);
 
-  scrpos->x = 0;
-  scrpos->y = 0;
-  init_board(board_ptr);
+  int a;
+  int b;
+  getyx(stdscr, a, b);
 
-  // printf("%c", board_ptr->cells[0][0]);
+  move_cursor(0, COLS/2 - 1);
+  addstr("Tic-Tac-Toe");
 
-  nodelay(stdscr, TRUE);
+  init_board(board);
+
+  // makes getch() non-blocking i.e. makes it so that it always gets a single character at a time, unbuffered
+
+  game_loop(board);
+
+  endwin();
+  return EXIT_SUCCESS;
+}
+
+void game_loop(Board *board) {
   int ch;
      for (;;) {
           if ((ch = getch()) == ERR) {
@@ -57,26 +64,27 @@ int main(int argc, char *argv[])
               */
           }
           else {
-              handle_input(ch, scrpos, board_ptr);
+              handle_input(ch, board);
               /* user has pressed a key ch
                ...
               */
           }
      }
-
-  endwin();
-  return EXIT_SUCCESS;
 }
 
 void render_board(Board *board) {
+    int CURSOR_X;
+    int CURSOR_Y;
+
+    getyx(stdscr, CURSOR_Y, CURSOR_X);
+
     for (int row = 0; row <= 2; row++)
     {
       for (int col = 0; col <= 2; col++)
       {
-        putchar(board->cells[row][col].mark);
+        move_cursor(CURSOR_Y + col, CURSOR_X + row);
+        addch(board->cells[row][col].mark);
       }
-      putchar(10); // LF
-      clear();
     }
     refresh();
 }
@@ -97,22 +105,22 @@ void update_cell(Board *board, int row, int col, char symbol) {
   }
 }
 
-void handle_input(int ch, Screen_Position *scrpos, Board *board_ptr) {
+void handle_input(int ch, Board *board) {
     switch (ch) {
       case KEY_UP:
-          move_cursor_up(scrpos);
+          move_cursor_up();
           break;
       case KEY_DOWN:
-          move_cursor_down(scrpos);
+          move_cursor_down();
           break;
       case KEY_LEFT:
-          move_cursor_left(scrpos);
+          move_cursor_left();
           break;
       case KEY_RIGHT:
-          move_cursor_right(scrpos);
+          move_cursor_right();
           break;
       default:
-          move_cursor(scrpos, 0, 0); 
+          render_board(board);
   }
 }
 
@@ -121,35 +129,42 @@ void init_ncurses() {
   cbreak();
   noecho();
   keypad(stdscr, true);
+  nodelay(stdscr, TRUE);
 }
 
-void move_cursor(Screen_Position *scrpos, int y, int x) {
-  scrpos->x = x;
-  scrpos->y = y;
-  move(scrpos->y, scrpos->x);
+void move_cursor(int y, int x) {
+  move(y, x);
   refresh();
 }
 
-void move_cursor_up(Screen_Position *scrpos) {
-  scrpos->y = scrpos->y - 1;
-  move(scrpos->y, scrpos->x);
-  refresh();
+void move_cursor_up() {
+  int CURSOR_Y;
+  int CURSOR_X;
+  getyx(stdscr, CURSOR_Y, CURSOR_X);
+
+  move_cursor(CURSOR_Y - 1, CURSOR_X);
 }
 
-void move_cursor_down(Screen_Position *scrpos) {
-  scrpos->y = scrpos->y + 1;
-  move(scrpos->y, scrpos->x);
-  refresh();
+void move_cursor_down() {
+  int CURSOR_Y;
+  int CURSOR_X;
+  getyx(stdscr, CURSOR_Y, CURSOR_X);
+
+  move_cursor(CURSOR_Y + 1, CURSOR_X);
 }
 
-void move_cursor_left(Screen_Position *scrpos) {
-  scrpos->x = scrpos->x - 1;
-  move(scrpos->y, scrpos->x);
-  refresh();
+void move_cursor_left() {
+  int CURSOR_Y;
+  int CURSOR_X;
+  getyx(stdscr, CURSOR_Y, CURSOR_X);
+
+  move_cursor(CURSOR_Y, CURSOR_X - 1);
 }
 
-void move_cursor_right(Screen_Position *scrpos) {
-  scrpos->x = scrpos->x + 1;
-  move(scrpos->y, scrpos->x);
-  refresh();
+void move_cursor_right() {
+  int CURSOR_Y;
+  int CURSOR_X;
+  getyx(stdscr, CURSOR_Y, CURSOR_X);
+
+  move_cursor(CURSOR_Y, CURSOR_X + 1);
 }
