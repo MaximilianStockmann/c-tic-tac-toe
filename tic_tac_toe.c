e19f14f13445ea6cf53618453fc4a41c6a542ca2
Maximilian Stockmann <maximilian.stockmann@posteo.de>
2024-07-29 15:59:20 +0100

    add ncurses init + basic cursor movement testing

--- tic_tac_toe.c
+++ tic_tac_toe.c
@@ -15,8 +15,16 @@ typedef struct {
   Cell cells[3][3];
 } Board;
 
+typedef struct {
+  int x;
+  int y;
+} Screen_Position;
+
 void render_board();
 void init_board();
+void update_cell(Board *board, int row, int col, char symbol);
+void init_ncurses();
+void move_cursor(int x, int y);
 
 // have a nine by nine array
 // render the array upon each input as some series of symbols
@@ -24,18 +32,36 @@ void init_board();
 // upon using one of the arrow keys change cursor location in array
 int main(int argc, char *argv[])
 {
+  init_ncurses();
   int c; /* the character read*/
   Board *board_ptr = malloc(sizeof *board_ptr);
+  Screen_Position *scrpos = malloc(sizeof *scrpos);
 
+  scrpos->x = 0;
+  scrpos->y = 0;
   init_board(board_ptr);
 
   // printf("%c", board_ptr->cells[0][0]);
 
-  while ((c = getchar()) != EOF)
-  {
-    render_board(board_ptr);
-  }
+  nodelay(stdscr, TRUE);
+  int ch;
+     for (;;) {
+          if ((ch = getch()) == ERR) {
+              /* user hasn't responded
+               ...
+              */
+          }
+          else {
+              scrpos->x += 1;
+              scrpos->y += 1;
+              move_cursor(scrpos->x,scrpos->y);
+              /* user has pressed a key ch
+               ...
+              */
+          }
+     }
 
+  endwin();
   return EXIT_SUCCESS;
 }
 
@@ -55,7 +81,27 @@ void render_board(Board *board) {
 void init_board(Board *board) {
   for (int row = 0; row <= 2; row++) {
     for (int col = 0; col <= 2; col++) {
-      board->cells[row][col].mark = X_MARK;
+      update_cell(board, row, col, O_MARK);
     }
   }
+}
+
+void update_cell(Board *board, int row, int col, char symbol) {
+  if (symbol == X_MARK || symbol == O_MARK) {
+    board->cells[row][col].mark = symbol;
+  } else {
+    printf("%s\n", "Not a valid symbol");
+  }
+}
+
+void init_ncurses() {
+  initscr();
+  cbreak();
+  noecho();
+  keypad(stdscr, true);
+}
+
+void move_cursor(int x, int y) {
+  move(x, y);
+  refresh();
 }
